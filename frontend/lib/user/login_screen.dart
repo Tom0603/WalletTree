d540b843909fef04ec45f3a663c7e047642a4adf
Tom <tom.schneider0603@gmail.com>
2022-10-31 20:04:06 +0000

    added candlestick chart icon for Login page

--- frontend/lib/user/login_screen.dart
+++ frontend/lib/user/login_screen.dart
@@ -14,118 +14,121 @@ class _LoginPageState extends State<LoginPage> {
       backgroundColor: Colors.grey[200],
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 300,
-              ),
-
-              // Hello Text
-              Text(
-                'Hello There!',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 36,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                    Icons.candlestick_chart_outlined,
+                    size: 140,
                 ),
-              ),
-              SizedBox(height: 25),
-              Text(
-                'Nice to see you here!',
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              const SizedBox(height: 120),
 
-              // Email input field
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      border: Border.all(color: Colors.deepPurple),
-                      borderRadius: BorderRadius.circular(12)),
-                  child: const Padding(
-                    padding: EdgeInsets.only(left: 20.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Email",
-                      ),
-                    ),
+                // Hello Text
+                Text(
+                  'Hello There!',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 36,
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-
-              // Password input field
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      border: Border.all(color: Colors.deepPurple),
-                      borderRadius: BorderRadius.circular(12)),
-                  child: const Padding(
-                    padding: EdgeInsets.only(left: 20.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Password",
-                      ),
-                    ),
+                SizedBox(height: 25),
+                Text(
+                  'Nice to see you here!',
+                  style: TextStyle(
+                    fontSize: 20,
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
+                const SizedBox(height: 120),
 
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                    padding: const EdgeInsets.all(20),
+                // Email input field
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
                     decoration: BoxDecoration(
-                      color: Colors.deepPurple,
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        'Sign in',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
+                        color: Colors.grey[200],
+                        border: Border.all(color: Colors.deepPurple),
+                        borderRadius: BorderRadius.circular(12)),
+                    child: const Padding(
+                      padding: EdgeInsets.only(left: 20.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Email",
                         ),
                       ),
-                    )),
-              ),
-              const SizedBox(height: 20),
-
-              // Not a member?
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Not a Member?',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  Text(
-                    ' Register Now!',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold, color: Colors.blue),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+
+                // Password input field
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        border: Border.all(color: Colors.deepPurple),
+                        borderRadius: BorderRadius.circular(12)),
+                    child: const Padding(
+                      padding: EdgeInsets.only(left: 20.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Password",
+                        ),
+                      ),
+                    ),
                   ),
-                ],
-              )
-            ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Sign in',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      )),
+                ),
+                const SizedBox(height: 20),
+
+                // Not a member?
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Not a Member?',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      ' Register Now!',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.blue),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
