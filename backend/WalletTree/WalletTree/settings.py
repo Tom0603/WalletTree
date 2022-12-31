7ba6462382de9e25fd8ce1045020b882f2bbef52
Tom <tom.schneider0603@gmail.com>
2022-12-31 15:37:55 +0100

    django "SECRET_KEY" is now secure again

--- backend/WalletTree/WalletTree/settings.py
+++ backend/WalletTree/WalletTree/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,8 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8sqo5vn#ub$#2#h+!m^#6k03dgqq-kbjc=0jv$k4f0$p5@13b$"
+# SECRET_KEY
+load_dotenv(find_dotenv())
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
