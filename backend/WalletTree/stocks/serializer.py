c58ee0a7e2243a3d7c9608093f32e0c90fac0ea5
Tom <tom.schneider0603@gmail.com>
2023-03-19 00:24:11 +0000

    added serializer for "Orders" model

---
+++ backend/WalletTree/stocks/serializer.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from .models import Orders
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Orders
+        fields = [
+            "user_email",
+            "ticker",
+            "shares",
+            "price"
+        ]
