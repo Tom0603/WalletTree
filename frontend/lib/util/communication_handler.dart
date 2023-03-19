71477e3769b3772c69176cfeb88c500438167ca9
Tom <tom.schneider0603@gmail.com>
2023-03-19 00:28:30 +0000

    created communication handler "OrdersHandler"

---
+++ frontend/lib/util/communication_handler.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:frontend/models/orders.dart';
+import 'package:http/http.dart' as http;
+
+const String urlPrefix = "http://127.0.0.1:8000/orderdetails";
+
+class OrdersHandler {
+  Future<List<Orders>> fetchOrders() async {
+    /// FOR DEBUG PURPOSES
+    print("fetching");
+
+    http.Response response = await http.get(Uri.parse(urlPrefix));
+
+    if (response.statusCode != 200) {
+      /// FOR DEBUG PURPOSES
+      print("Exception");
+
+      throw Exception("Error, ${response.statusCode}");
+    } else {
+      /// FOR DEBUG PURPOSES
+      print("Success");
+
+      List<dynamic> jsonResponse = jsonDecode(response.body);
+      List<Orders> orders = [];
+      jsonResponse.map((json) {
+        orders.add(Orders.fromJson(json));
+      }).toList();
+      return orders;
+    }
+  }
+}
